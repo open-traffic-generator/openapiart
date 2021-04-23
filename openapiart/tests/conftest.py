9e14a931066081b5e30e20d6a6a8e2a09ff3a5af
ajbalogh <andy.balogh@keysight.com>
2021-04-23 09:26:29 -0700

    update fixture docstring

--- openapiart/tests/conftest.py
+++ openapiart/tests/conftest.py
@@ -5,20 +5,27 @@ import importlib
 
 
 @pytest.fixture(scope='session')
-def generator():
+def openapiart():
+    """Return an instance of the OpenApiArt class
+
+    Instantiating the OpenApiArt class generates OpenAPI artifacts and an 
+    enhanced ux python package.
+    """
     api_files = [
         os.path.join(os.path.dirname(__file__), './api/info.yaml'),
         os.path.join(os.path.dirname(__file__), './api/api.yaml')
     ]
     sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..')))
     module = importlib.import_module('openapiart.openapiart')
-    generator = getattr(module, 'OpenApiArt')(api_files=api_files)
-    return generator
+    openapiart = getattr(module, 'OpenApiArt')(api_files=api_files)
+    return openapiart
 
 
 @pytest.fixture
-def api(generator):
-    sys.path.append(generator.output_dir)
-    module = importlib.import_module(generator.python_module_name)
-    pkg = getattr(module, generator.python_module_name)
-    return pkg.api()
+def api(openapiart):
+    """Return an instance of the top level Api class from the generated package
+    """
+    sys.path.append(openapiart.output_dir)
+    module = importlib.import_module(openapiart.python_module_name)
+    package = getattr(module, openapiart.python_module_name)
+    return package.api()
