50fd16f75759317e788845565739f33820dc9ee6
KEYSIGHT\jstianse <jesper.kristiansen@keysight.com>
2021-07-28 10:18:35 -0700

    close issue #54. Remove item

---
+++ openapiart/tests/test_remove.py
@@ -0,0 +1,23 @@
+import pytest
+import jsonpath_ng as jp
+import json
+
+def test_remove(api):
+    config = api.prefix_config()
+    config.g.add(name="a")
+    config.g.add(name="b")
+    config.g.add(name="c")
+    assert len(config.g) == 3
+    assert config.g[0].name == 'a'
+    assert config.g[1].name == 'b'
+    assert config.g[2].name == 'c'
+
+    config.g.remove(1)
+    config.g.add(name="d")
+    assert len(config.g) == 3
+    assert config.g[0].name == 'a'
+    assert config.g[1].name == 'c'
+    assert config.g[2].name == 'd'
+
+if __name__ == "__main__":
+    pytest.main(["-v", "-s", __file__])
