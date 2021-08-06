62b5a3556bdae130ad4b3f2d3b40d97ae1a9a28d
Automation <anish.gottapu@keysight.com>
2021-08-06 14:42:15 +0530

    tests for generated protobuf

--- openapiart/tests/conftest.py
+++ openapiart/tests/conftest.py
@@ -3,6 +3,7 @@ import sys
 import os
 import importlib
 import logging
+from .utils import common as utl
 
 
 @pytest.fixture(scope="session")
@@ -40,6 +41,8 @@ def api(openapiart):
     # package = getattr(module, openapiart.python_module_name)
     pytest.server = OpenApiServer(module).start()
     return module.api(location='http://127.0.0.1:80', verify=False, logger=None, loglevel=logging.DEBUG)
+    # return module.api(
+    #     location=None, verify=False, logger=None, loglevel=logging.DEBUG)
 
 
 @pytest.fixture
@@ -55,3 +58,39 @@ def default_config(api):
     config.b = 1.1
     config.c = 1
     return config
+def pb_config(pb2):
+    """ Returns an instance of config of *_pb2 package"""
+    return pb2.PrefixConfig()
+
+
+@pytest.fixture
+def utils():
+    return utl
+
+
+@pytest.fixture(scope="session")
+def pb2(openapiart):
+    """Returns pb2 package"""
+    path = os.path.join(
+        openapiart.output_dir, openapiart.python_module_name)
+    if path not in sys.path:
+        sys.path.append(path)
+    return importlib.import_module(openapiart.python_module_name + "_pb2")
+
+
+@pytest.fixture(scope="session")
+def pb2_grpc(openapiart):
+    """Returns pb2_grpc package"""
+    path = os.path.join(
+        openapiart.output_dir, openapiart.python_module_name)
+    if path not in sys.path:
+        sys.path.append(path)
+    return importlib.import_module(openapiart.python_module_name + "_pb2_grpc")
+
+
+@pytest.fixture(scope="session")
+def grpc_api(pb2, pb2_grpc):
+    """grpc API"""
+    from .grpcserver import web_server
+    grpc_api = web_server(pb2, pb2_grpc).start()
+    yield grpc_api
