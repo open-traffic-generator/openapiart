d2735d3a833929c9b413ef2b57127280ed92d3eb
ajbalogh <andy.balogh@keysight.com>
2021-08-09 15:01:22 -0700

    add getter/setter generation

--- openapiart/openapiartgo.py
+++ openapiart/openapiartgo.py
@@ -3,6 +3,45 @@ import os
 import subprocess
 
 
+class FluentStructure(object):
+    def __init__(self):
+        self.internal_struct_name = None
+        self.external_interface_name = None
+        self.external_new_methods = []
+        self.external_rpc_methods = []
+        self.components = {}
+
+
+class FluentRpc(object):
+    """SetConfig(config Config) error"""
+
+    def __init__(self):
+        self.operation_name = None
+        self.method = None
+        self.request = None
+
+
+class FluentNew(object):
+    """New<external_interface_name> <external_interface_name>"""
+
+    def __init__(self):
+        self.generated = False
+        self.interface = None
+        self.struct = None
+        self.method = None
+        self.schema_name = None
+        self.schema_object = None
+        self.interface_fields = []
+
+
+class FluentField(object):
+    def __init__(self):
+        self.name = None
+        self.type = None
+        self.setter_method = None
+        self.getter_method = None
+
+
 class OpenApiArtGo(OpenApiArtPlugin):
     """Generates a fluent interface go package that encapsulates protoc
     generated .pg.go and _grpc.pb.go content
@@ -50,6 +89,7 @@ class OpenApiArtGo(OpenApiArtPlugin):
 
     def __init__(self, **kwargs):
         super(OpenApiArtGo, self).__init__(**kwargs)
+        self._api = FluentStructure()
         self._api_interface_methods = []
         self._oapi_go_types = {
             "string": "string",
@@ -89,13 +129,9 @@ class OpenApiArtGo(OpenApiArtPlugin):
         self._write_package_docstring(self._openapi["info"])
         self._write_package()
         self._write_common_code()
-        self._write_api_interface()
-        # self._write_response_msg(path_object)
-        # for name, schema_object in self._openapi["components"]["schemas"].items():
-        #     self._write_struct(name, schema_object)
-        # for name, response_object in self._openapi["components"]["responses"].items():
-        #     self._write_msg(name, response_object)
-        # self._write_types()
+        self._build_api_interface()
+        self._build_request_interfaces()
+        self._build_component_interfaces()
         self._close_fp()
 
     def _write_package_docstring(self, info_object):
@@ -229,16 +265,35 @@ class OpenApiArtGo(OpenApiArtPlugin):
     def _get_external_field_name(self, openapi_name):
         return self._get_external_name(openapi_name) + "_"
 
-    def _write_api_interface(self):
-        self._internal_api_struct_name = f"""{self._get_internal_name(self._go_module_name)}Api"""
+    def _build_api_interface(self):
+        self._api.internal_struct_name = f"""{self._get_internal_name(self._go_module_name)}Api"""
+        self._api.external_interface_name = f"""{self._get_external_name(self._go_module_name)}Api"""
+        for _, path_object in self._openapi["paths"].items():
+            for _, path_item_object in path_object.items():
+                ref = self._get_parser("$..requestBody..'$ref'").find(path_item_object)
+                if len(ref) == 1:
+                    new = FluentNew()
+                    new.schema_name = self._get_schema_object_name_from_ref(ref[0].value)
+                    new.schema_object = self._get_schema_object_from_ref(ref[0].value)
+                    new.interface = self._get_external_name(new.schema_name)
+                    new.struct = self._get_internal_name(new.schema_name)
+                    new.method = f"""New{new.interface}() {new.interface}"""
+                    self._api.external_new_methods.append(new)
+                    rpc = FluentRpc()
+                    rpc.operation_name = self._get_external_name(path_item_object["operationId"])
+                    rpc.method = f"""{rpc.operation_name}({new.struct} {new.interface}) error"""
+                    rpc.request = f"""{self._protobuf_package_name}.Set{rpc.operation_name}Request{{{new.interface}: {new.struct}.msg()}}"""
+                    self._api.external_rpc_methods.append(rpc)
+
+        # write the go code
         self._write(
-            f"""type {self._internal_api_struct_name} struct {{
+            f"""type {self._api.internal_struct_name} struct {{
                 api
                 grpcClient {self._protobuf_package_name}.OpenapiClient
             }}
 
             // grpcConnect builds up a grpc connection
-            func (api *{self._internal_api_struct_name}) grpcConnect() error {{
+            func (api *{self._api.internal_struct_name}) grpcConnect() error {{
                 if api.grpcClient == nil {{
                     conn, err := grpc.Dial(api.grpcLocation, grpc.WithInsecure())
                     if err != nil {{
@@ -248,8 +303,10 @@ class OpenApiArtGo(OpenApiArtPlugin):
                 }}
                 return nil
             }}
-            func NewApi() *{self._internal_api_struct_name} {{
-                api := {self._internal_api_struct_name}{{}}
+
+            // NewApi returns a new instance of the top level interface hierarchy
+            func NewApi() *{self._api.internal_struct_name} {{
+                api := {self._api.internal_struct_name}{{}}
                 api.transport = ApiTransport.GRPC
                 api.grpcLocation = "127.0.0.1:5050"
                 api.grpcRequestTimeout = 10 * time.Second
@@ -258,110 +315,115 @@ class OpenApiArtGo(OpenApiArtPlugin):
             }}
             """
         )
-        api_interface_methods = []
-        delayed_writes = []
-        child_refs = []
-        for _, path_object in self._openapi["paths"].items():
-            self._write_request_struct(path_object, api_interface_methods, delayed_writes, child_refs)
-        methods = "\n".join(api_interface_methods)
-        self._external_api_interface_name = f"""{self._get_external_name(self._go_module_name)}Api"""
+        methods = []
+        for new in self._api.external_new_methods:
+            methods.append(new.method)
+        for rpc in self._api.external_rpc_methods:
+            methods.append(rpc.method)
+        method_signatures = "\n".join(methods)
         self._write(
-            f"""type {self._external_api_interface_name} interface {{
+            f"""type {self._api.external_interface_name} interface {{
                 Api
-                {methods}
+                {method_signatures}
             }}
             """
         )
-        for item in delayed_writes:
-            self._write(item)
-        for ref in child_refs:
-            delayed_writes = []
-            schema_object = self._get_schema_object_from_ref(ref)
-            self._write_struct_and_interface(schema_object, delayed_writes)
-            for delay in delayed_writes:
-                self._write(delay)
+        for new in self._api.external_new_methods:
+            self._write(
+                f"""func (api *{self._api.internal_struct_name}) {new.method} {{
+                    return &{new.struct}{{}}
+                }}
+                """
+            )
+        for rpc in self._api.external_rpc_methods:
+            self._write(
+                f"""func (api *{self._api.internal_struct_name}) {rpc.method} {{
+                    if err := api.grpcConnect(); err != nil {{
+                        return err
+                    }}
+                    request := {rpc.request}
+                    ctx, cancelFunc := context.WithTimeout(context.Background(), api.grpcRequestTimeout)
+                    defer cancelFunc()
+                    client, err := api.grpcClient.{rpc.operation_name}(ctx, &request)
+                    if err != nil {{
+                        return err
+                    }}
+                    resp, _ := client.Recv()
+                    if resp.GetStatusCode_200() == nil {{
+                        return fmt.Errorf("fail")
+                    }}
+                    return nil
+                }}
+                """
+            )
 
-    def _write_request_struct(self, path_object, api_interface_methods, delayed_writes, child_refs):
-        """Write the operation struct and func, add the func signature to api_interfaces,
-        return the api_interfaces
-        type <InternalStructName> struct {
-            <self._protobuf_package_name>.<ExternalStructName>
-        }
-        func (api *api) <operationid>(request *<structname>) response *structname {
-        }
+    def _build_request_interfaces(self):
+        for new in self._api.external_new_methods:
+            self._build_interface(new)
+
+    def _build_component_interfaces(self):
+        while True:
+            components = [component for _, component in self._api.components.items() if component.generated is False]
+            if len(components) == 0:
+                break
+            for component in components:
+                self._build_interface(component)
+
+    def _build_interface(self, new):
+        self._write(
+            f"""type {new.struct} struct {{
+                {self._protobuf_package_name}.{new.interface}
+            }}
         """
-        for _, path_item_object in path_object.items():
-            operation_id = path_item_object["operationId"]
-            ref = self._get_parser("$..requestBody..'$ref'").find(path_item_object)
-            if len(ref) == 1:
-                schema_object_name = self._get_schema_object_name_from_ref(ref[0].value)
-                internal_struct_name = self._get_internal_name(schema_object_name)
-                external_struct_name = self._get_external_name(schema_object_name)
-                delayed_writes.append(
-                    f"""type {internal_struct_name} struct {{
-                        {self._protobuf_package_name}.{external_struct_name}
-                    }}
+        )
+        self._build_setters_getters(new)
+        interfaces = []
+        for field in new.interface_fields:
+            interfaces.append(field.setter_method)
+            interfaces.append(field.getter_method)
+        interface_signatures = "\n".join(interfaces)
+        self._write(
+            f"""type {new.interface} interface {{
+                msg() *{self._protobuf_package_name}.{new.struct}
+                {interface_signatures}
+            }}
+        """
+        )
+        self._write(
+            f"""func (obj *{new.struct}) msg() *{self._protobuf_package_name}.{new.interface} {{
+                return &obj.{new.interface}
+            }}
+        """
+        )
+        for field in new.interface_fields:
+            self._write(
+                f"""func (obj *{new.struct}) {field.setter_method} {{
+                    obj.{new.interface}.{field.name} = value
+                    return obj
+                }}
                 """
-                )
-                # this writes the interface which needs to contain all the properties
-                # foreach property generate the struct/interface and add it to delayed writes
-                # TBD: have write_struct_and_interface add any refs that have been generated to delayed_refs
-                schema_object = self._get_schema_object_from_ref(ref[0].value)
-                interface_methods = self._write_struct_and_interface(schema_object, delayed_writes, child_refs)
-                interfaces = "\n".join(interface_methods)
-                delayed_writes.append(
-                    f"""type {external_struct_name} interface {{
-                        msg() *{self._protobuf_package_name}.{external_struct_name}
-                        {interfaces}
-                    }}
+            )
+            self._write(
+                f"""func (obj *{new.struct}) {field.getter_method} {{
+                    return obj.{new.interface}.{field.name}
+                }}
                 """
-                )
-                delayed_writes.append(
-                    f"""func (obj *{internal_struct_name}) msg() *{self._protobuf_package_name}.{external_struct_name} {{
-                        return &obj.{external_struct_name}
-                    }}
-                """
-                )
-                external_operation_name = self._get_external_name(operation_id)
-                external_method = f"""{external_operation_name}({internal_struct_name} {external_struct_name}) error"""
-                delayed_writes.append(
-                    f"""func (api *{self._go_module_name}Api) {external_method} {{
-                        if err := api.grpcConnect(); err != nil {{
-                            return err
-                        }}
-                        request := {self._protobuf_package_name}.{external_operation_name}Request{{{external_struct_name}: {internal_struct_name}.msg()}}
-                        ctx, cancelFunc := context.WithTimeout(context.Background(), api.grpcRequestTimeout)
-                        defer cancelFunc()
-                        client, err := api.grpcClient.{external_operation_name}(ctx, &request)
-                        if err != nil {{
-                            return err
-                        }}
-                        resp, _ := client.Recv()
-                        if resp.GetStatusCode_200() == nil {{
-                            return fmt.Errorf("fail")
-                        }}
-                        return nil
-                    }}
-                """
-                )
-                api_interface_methods.append(f"New{external_struct_name}() {external_struct_name}")
-                api_interface_methods.append(external_method)
+            )
+        new.generated = True
 
-    def _write_struct_and_interface(self, schema_object, delayed_writes, child_refs):
+    def _build_setters_getters(self, fluent_new):
         """Return"""
-        interface_methods = []
-        for property_name, property_schema in schema_object["properties"].items():
-            external_struct_name = self._get_external_name(property_name)
-            interface_methods.append(f"{external_struct_name}() string")
-            interface_methods.append(f"Set{external_struct_name}(value string)")
-        return interface_methods
+        for property_name, property_schema in fluent_new.schema_object["properties"].items():
+            field = FluentField()
+            field.name = self._get_external_name(property_name)
+            field.type = self._get_struct_field_type(property_schema)
+            field.getter_method = f"{field.name}() {field.type}"
+            field.setter_method = f"Set{field.name}(value {field.type}) {fluent_new.interface}"
+            fluent_new.interface_fields.append(field)
 
     def _get_schema_object_name_from_ref(self, ref):
         final_piece = ref.split("/")[-1]
-        if "." in final_piece:
-            return final_piece.split(".")[-1]
-        else:
-            return final_piece
+        return final_piece.replace(".", "")
 
     def _get_schema_object_from_ref(self, ref):
         leaf = self._openapi
@@ -429,19 +491,30 @@ class OpenApiArtGo(OpenApiArtPlugin):
 
     def _get_struct_field_type(self, property_schema, required=False):
         """Convert openapi type, format, items, $ref keywords to a go type"""
-        pointer = "" if required else "*"
         go_type = ""
         if "type" in property_schema:
             oapi_type = property_schema["type"]
             if oapi_type == "array":
-                go_type += f"{pointer}[]" + self._get_struct_field_type(property_schema["items"]).replace("*", "")
+                go_type += f"[]" + self._get_struct_field_type(property_schema["items"]).replace("*", "")
             else:
                 if "format" in property_schema:
                     oapi_type += property_schema["format"]
-                go_type = f"{pointer}{self._oapi_go_types[oapi_type.lower()]}"
+                go_type = f"{self._oapi_go_types[oapi_type.lower()]}"
         elif "$ref" in property_schema:
-            ref = property_schema["$ref"].split("/")[-1]
-            go_type = f"{pointer}{self._get_internal_name(ref)}"
+            ref = property_schema["$ref"]
+            schema_object_name = self._get_schema_object_name_from_ref(ref)
+            schema_object = self._get_schema_object_from_ref(ref)
+            new = None
+            if schema_object_name in self._api.components:
+                new = self._api.components[schema_object_name]
+            else:
+                new = FluentNew()
+                new.schema_object = schema_object
+                new.schema_name = schema_object_name
+                new.struct = self._get_internal_name(schema_object_name)
+                new.interface = self._get_external_name(schema_object_name)
+                self._api.components[new.schema_name] = new
+            go_type = new.interface
         else:
             raise Exception(f"No type or $ref keyword present in property schema: {property_schema}")
         return go_type
