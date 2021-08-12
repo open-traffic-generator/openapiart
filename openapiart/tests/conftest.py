e85df99fcf1536edec08cc1b520c191cf85aaaa2
ajbalogh <andy.balogh@keysight.com>
2021-08-12 09:47:24 -0700

    fix build

--- openapiart/tests/conftest.py
+++ openapiart/tests/conftest.py
@@ -6,40 +6,15 @@ import logging
 
 
 @pytest.fixture(scope="session")
-def openapiart():
-    """Return an instance of the OpenApiArt class
-
-    Instantiating the OpenApiArt class generates OpenAPI artifacts and an
-    enhanced ux python package.
-    """
-    sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..")))
-    api_files = [
-        os.path.join(os.path.dirname(__file__), "./api/info.yaml"),
-        os.path.join(os.path.dirname(__file__), "./common/common.yaml"),
-        os.path.join(os.path.dirname(__file__), "./api/api.yaml"),
-    ]
-    module = importlib.import_module("openapiart.openapiart")
-    openapiart_class = getattr(module, "OpenApiArt")
-    openapiart = openapiart_class(
-        api_files=api_files,
-        output_dir="./art",
-        python_module_name="sanity",
-        protobuf_file_name="sanity",
-        protobuf_package_name="sanity",
-        go_module_name="openapiart",
-        extension_prefix="sanity",
-    )
-    return openapiart
-
-
-@pytest.fixture(scope="session")
-def api(openapiart):
+def api():
     """Return an instance of the top level Api class from the generated package"""
     from .server import OpenApiServer
 
-    sys.path.append(openapiart.output_dir)
-    module = importlib.import_module(openapiart.python_module_name)
-    # package = getattr(module, openapiart.python_module_name)
+    # TBD: fix this hardcoding
+    # artifacts should not be generated from here as these tests are run as sudo
+    sys.path.append("./art")
+    module = importlib.import_module("sanity")
+
     pytest.server = OpenApiServer(module).start()
     return module.api(location="http://127.0.0.1:80", verify=False, logger=None, loglevel=logging.DEBUG)
 
