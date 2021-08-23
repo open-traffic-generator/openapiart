52da5665faf400caea5e2793271387fe79ea8150
ajbalogh <andy.balogh@keysight.com>
2021-08-23 12:36:17 -0700

    fix python ux sdk validation of property objects that are required

--- openapiart/tests/server.py
+++ openapiart/tests/server.py
@@ -10,39 +10,31 @@ import pytest
 
 
 app = Flask(__name__)
-CONFIG = None
+app.CONFIG = None
+app.PACKAGE = None
 
-@app.route('/config', methods=['POST'])
+
+@app.route("/config", methods=["POST"])
 def set_config():
-    global CONFIG
-    # TODO Shall change the below sanity path to be dynamic
-    pkg_name = 'sanity'
-    lib_path = '../../.output/openapiart/%s' % pkg_name
-    sys.path.append(os.path.join(os.path.join(os.path.dirname(__file__), lib_path)))
-    package = importlib.import_module(pkg_name)
-    config = package.Api().prefix_config()
-    config.deserialize(request.data.decode('utf-8'))
+    config = app.PACKAGE.Api().prefix_config()
+    config.deserialize(request.data.decode("utf-8"))
     test = config.h
     if test is not None and isinstance(test, bool) is False:
-        return Response(status=590,
-                        response=json.dumps({'detail': 'invalid data type'}),
-                        headers={'Content-Type': 'application/json'})
+        return Response(status=590, response=json.dumps({"detail": "invalid data type"}), headers={"Content-Type": "application/json"})
     else:
-        CONFIG = config
+        app.CONFIG = config
         return Response(status=200)
 
 
-@app.route('/config', methods=['GET'])
+@app.route("/config", methods=["GET"])
 def get_config():
-    global CONFIG
-    return Response(CONFIG.serialize() if CONFIG is not None else '{}',
-                    mimetype='application/json',
-                    status=200)
+    serialized_config = app.CONFIG.serialize()
+    return Response(serialized_config, mimetype="application/json", status=200)
 
 
 @app.after_request
 def after_request(resp):
-    print(request.method, request.url, ' -> ', resp.status)
+    print(request.method, request.url, " -> ", resp.status)
     return resp
 
 
@@ -52,8 +44,12 @@ def web_server():
 
 class OpenApiServer(object):
     def __init__(self, package):
-        self._CONFIG = None
-        self.package = package
+        # TODO Shall change the below sanity path to be dynamic
+        pkg_name = "sanity"
+        lib_path = "../../.output/openapiart/%s" % pkg_name
+        sys.path.append(os.path.join(os.path.join(os.path.dirname(__file__), lib_path)))
+        app.PACKAGE = importlib.import_module(pkg_name)
+        app.CONFIG = app.PACKAGE.Api().prefix_config()
 
     def start(self):
         self._web_server_thread = threading.Thread(target=web_server)
@@ -63,11 +59,11 @@ class OpenApiServer(object):
         return self
 
     def _wait_until_ready(self):
-        api = self.package.api(location='http://127.0.0.1:80')
+        api = app.PACKAGE.api(location="http://127.0.0.1:80")
         while True:
             try:
                 api.get_config()
                 break
-            except Exception:
-                pass
-            time.sleep(.1)
+            except Exception as e:
+                print(e)
+            time.sleep(0.1)
