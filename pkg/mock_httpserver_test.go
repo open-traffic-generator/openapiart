f3e20de06d0505dc2ebf7799b0edbaf322760087
ajbalogh <andy.balogh@keysight.com>
2021-08-25 18:28:45 -0700

    update mock httpserver

--- pkg/mock_httpserver_test.go
+++ pkg/mock_httpserver_test.go
@@ -5,39 +5,44 @@ import (
 	"log"
 	"net/http"
 
-	. "github.com/open-traffic-generator/openapiart/pkg"
+	art "github.com/open-traffic-generator/openapiart/pkg"
 )
 
-type ResponseWarning struct {
-	Warnings []string
+type HttpServer struct {
+	Api    art.OpenapiartApi
+	Config art.PrefixConfig
 }
 
+var (
+	httpServer HttpServer = HttpServer{}
+)
+
 func StartMockHttpServer() {
+	httpServer.Api = art.NewApi()
+	httpServer.Config = httpServer.Api.NewPrefixConfig()
+
 	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
-		api := NewApi()
 		switch r.Method {
 		case http.MethodPost:
 			body, _ := ioutil.ReadAll(r.Body)
-			request := api.NewPrefixConfig()
-			request.FromJson(string(body))
-			response := api.NewSetConfigResponse_StatusCode200()
+			httpServer.Config.FromJson(string(body))
+			response := httpServer.Api.NewSetConfigResponse_StatusCode200()
 			response.SetBytes([]byte("Successful set config operation"))
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(response.ToJson()))
 		case http.MethodPatch:
 			body, _ := ioutil.ReadAll(r.Body)
-			request := api.NewPrefixConfig()
+			request := httpServer.Api.NewPrefixConfig()
 			request.FromJson(string(body))
-			response := api.NewUpdateConfigResponse_StatusCode200()
+			response := httpServer.Api.NewUpdateConfigResponse_StatusCode200()
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(response.ToJson()))
 		case http.MethodGet:
-			body, _ := ioutil.ReadAll(r.Body)
-			request := api.NewPrefixConfig()
-			request.FromJson(string(body))
-			response := api.NewGetConfigResponse_StatusCode200()
+			config := httpServer.Api.NewPrefixConfig()
+			response := httpServer.Api.NewGetConfigResponse_StatusCode200()
+			response.PrefixConfig().FromJson(config.ToJson())
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(response.ToJson()))
