0ac013ae29e14620a33169ae4d607a3d587cbac4
Rangababu-R <72373312+Rangababu-R@users.noreply.github.com>
2021-09-09 10:52:14 +0530

    Validation

--- openapiart/common.go
+++ openapiart/common.go
@@ -126,11 +126,110 @@ type httpClient struct {
 // All api rpcs MUST call Validate
 var validation []string
 
-func Validate() {
+func validationResult() {
 	if len(validation) > 0 {
 		for _, item := range validation {
 			fmt.Println(item)
 		}
 		panic("validation errors")
 	}
+}
+
+func validateMac(mac string) error {
+	macSlice := strings.Split(mac, ":")
+	if len(macSlice) != 6 {
+		return errors.New(fmt.Sprintf("Invalid Mac address %s", mac))
+	}
+	octInd := []string{"0th", "1st", "2nd", "3rd", "4th", "5th"}
+	for ind, val := range macSlice {
+		num, err := strconv.ParseUint(val, 16, 32)
+		if err != nil || num < 0 || num > 255 {
+			return errors.New(fmt.Sprintf("Invalid Mac address at %s octet in %s mac", octInd[ind], mac))
+		}
+	}
+	return nil
+}
+
+func validateIpv4(ip string) error {
+	ipSlice := strings.Split(ip, ".")
+	if len(ipSlice) != 4 {
+		return errors.New(fmt.Sprintf("Invalid Ipv4 address %s", ip))
+	}
+	octInd := []string{"1st", "2nd", "3rd", "4th"}
+	for ind, val := range ipSlice {
+		num, err := strconv.ParseUint(val, 10, 32)
+		if err != nil || num < 0 || num > 255 {
+			return errors.New(fmt.Sprintf("Invalid Ipv4 address at %s octet in %s ipv4", octInd[ind], ip))
+		}
+	}
+	return nil
+}
+
+func validateIpv6(ip string) error {
+	ip = strings.Trim(ip, " \t")
+	if strings.Count(ip, " ") > 0 || strings.Count(ip, ":") > 7 ||
+		strings.Count(ip, "::") > 1 || strings.Count(ip, ":::") > 0 ||
+		strings.Count(ip, ":") == 0 {
+		return errors.New(fmt.Sprintf("Invalid ipv6 address %s", ip))
+	}
+	if (string(ip[0]) == ":" && string(ip[:2]) != "::") || (string(ip[len(ip)-1]) == ":" && string(ip[len(ip)-2:]) != "::") {
+		return errors.New(fmt.Sprintf("Invalid ipv6 address %s", ip))
+	}
+	if strings.Count(ip, "::") == 0 && strings.Count(ip, ":") != 7 {
+		return errors.New(fmt.Sprintf("Invalid ipv6 address %s", ip))
+	}
+	if ip == "::" {
+		return nil
+	}
+	if ip[:2] == "::" {
+		r := strings.NewReplacer("::", "0:")
+		ip = r.Replace(ip)
+	} else if ip[len(ip)-2:] == "::" {
+		r := strings.NewReplacer("::", ":0")
+		ip = r.Replace(ip)
+	} else {
+		r := strings.NewReplacer("::", ":0:")
+		ip = r.Replace(ip)
+	}
+	octInd := []string{"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"}
+
+	ipSlice := strings.Split(ip, ":")
+
+	for ind, val := range ipSlice {
+		num, err := strconv.ParseUint(val, 16, 64)
+		if err != nil || num < 0 || num > 65535 {
+			return errors.New(fmt.Sprintf("Invalid Ipv4 address at %s octet in %s ipv4", octInd[ind], ip))
+		}
+	}
+
+	return nil
+}
+
+func validateHex(hex string) error {
+	return nil
+}
+
+func validateMacSlice(mac []string) error {
+	indices := []string{}
+
+	for i, m := range mac {
+		err := ValidateMac(m)
+		if err != nil {
+			indices = append(indices, string(i))
+		}
+	}
+	if len(indices) > 0 {
+		return errors.New(
+			fmt.Sprintf("Invalid Mac addresses at indices %s", strings.Join(indices, ",")),
+		)
+	}
+	return nil
+}
+
+func validateIpv4Slice(ip []string) error {
+	return nil
+}
+
+func validateIpv6Slice(ip []string) error {
+	return nil
 }
