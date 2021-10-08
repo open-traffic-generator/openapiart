ec7b8a2bde7bab78686334e955633588708affb1
ajbalogh <30300566+ajbalogh@users.noreply.github.com>
2021-10-08 16:03:30 -0700

    v0.1.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "openapiart"
-version = "0.1.17"
+version = "0.1.18"
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(base_dir, "README.md")) as fid:
