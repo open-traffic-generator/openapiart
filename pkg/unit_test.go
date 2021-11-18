0652e9456d506a3861355729850f8693119fbe82
alakendu <alakendu.jana@keysight.com>
2021-11-18 11:26:00 +0530

    modify RequiredObject UT

--- pkg/unit_test.go
+++ pkg/unit_test.go
@@ -707,16 +707,12 @@ func TestFromJsonToCleanObject(t *testing.T) {
 	new_json1 := `{
 		"b" : 65, 
 		"c" : 33,
-		"response" : "status_200", 
-		"required_object" : {
-			"e_a" : 1, 
-			"e_b" : 2
-		},
+		"response" : "status_200",
 		"h": false
 	}`
 	err1 := config.FromJson(new_json1)
 	assert.NotNil(t, err1)
-	assert.Contains(t, err1.Error(), "A is required field")
+	assert.Contains(t, err1.Error(), "RequiredObject is required field on interface PrefixConfig")
 }
 
 func TestChoiceStale(t *testing.T) {
