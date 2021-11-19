6defb9f4d09703e8c83e45dd6718a46ca5e72fee
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2021-11-19 15:06:52 +0530

    update do.py

--- do.py
+++ do.py
@@ -171,7 +171,7 @@ def go_lint():
     run(["curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0"])
     os.chdir("pkg")
     # run(["golangci-lint run -v"])
-    run(["golangci-lint run -v --modules-download-mode mod --skip-files gosnappi"])
+    run(["golangci-lint run -v --modules-download-mode mod"])
 
 def dist():
     clean()
