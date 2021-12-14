7a4ceef273075303bee86dd7c5fad6b34aa739ad
alakjana <alakendu.jana@keysight.com>
2021-12-14 17:33:42 +0530

    add go files

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
     ],
     keywords="testing openapi artifact generator",
-    package_data={"openapiart": ["*.go"]},
+    package_data={"openapiart": ["*.go", "goserver/*.go"]},
     include_package_data=True,
     packages=[pkg_name],
     python_requires=">=2.7, <4",
