d04b490ccb11626164f3e61d8b8a413f8c9fd475
Rangababu-R <rangababu.r@keysight.com>
2022-04-18 15:15:34 +0530

    unit tests

---
+++ openapiart/tests/test_auto.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_auto(config):
+    try:
+        config.auto_field_test.auto = 10
+        pytest.fail("able to set the auto field")
+    except Exception:
+        pass
+
+    assert config.auto_field_test.auto == 0
+
+    dt = config.serialize(config.DICT)
+
+    assert dt.get("auto_field_test", {}).get("choice") == "auto"
+    assert dt.get("auto_field_test", {}).get("auto") == 0
