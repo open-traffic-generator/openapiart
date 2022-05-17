0b4dd5c065cebd2595a0446714de4d1ebe1aaf01
alakjana <alakendu.jana@keysight.com>
2022-05-17 15:31:12 +0530

    add comments

--- openapiart/common.go
+++ openapiart/common.go
@@ -17,13 +17,21 @@ type grpcTransport struct {
 }
 
 type GrpcTransport interface {
+    // SetLocation set client connection to the given grpc target
 	SetLocation(value string) GrpcTransport
+	// Location get grpc target
 	Location() string
+    // SetRequestTimeout set timeout in grpc request
 	SetRequestTimeout(value time.Duration) GrpcTransport
+    // RequestTimeout get timeout in grpc request
 	RequestTimeout() time.Duration
+    // SetDialTimeout set timeout in grpc dial
 	SetDialTimeout(value time.Duration) GrpcTransport
+	// DialTimeout get timeout in grpc dial
 	DialTimeout() time.Duration
+	// SetClientConnection set grpc DialContext
 	SetClientConnection(con *grpc.ClientConn) GrpcTransport
+	// ClientConnection get grpc DialContext
 	ClientConnection() *grpc.ClientConn
 }
 
