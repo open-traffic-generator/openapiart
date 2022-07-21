50ed55fc1ee0debbf481b25de0329072df442720
ASHNA <ashna.aggarwal@keysight.com>
2022-07-21 16:45:30 +0530

    Update test_log.py

--- openapiart/tests/test_log.py
+++ openapiart/tests/test_log.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "art"))
 import sanity
 
+
 def test_log():
     sanity_api = sanity.api()
     sanity_api.logger.info("Testing logger")
