924b93a5de056e2b9e276e12fa7db78abb85ddd6
ASHNA <ashna.aggarwal@keysight.com>
2022-07-28 10:27:35 +0530

    lint issue fix

--- openapiart/generator.py
+++ openapiart/generator.py
@@ -446,8 +446,7 @@ class Generator:
                         ),
                     )
                     self._write(
-                        2,
-                        """self._logger.debug(f"Request data: {req_obj}")"""
+                        2, """self._logger.debug(f"Request data: {req_obj}")"""
                     )
                     self._write(2, "stub = self._get_stub()")
                     self._write(
@@ -462,9 +461,8 @@ class Generator:
                 self._write(3, "res_obj, preserving_proto_field_name=True")
                 self._write(2, ")")
                 self._write(
-                        2,
-                        """self._logger.debug(f"Response data: {response}")"""
-                    )
+                    2, """self._logger.debug(f"Response data: {response}")"""
+                )
                 self._write(
                     2, 'status_code_200 = response.get("status_code_200")'
                 )
--- openapiart/tests/test_log.py
+++ openapiart/tests/test_log.py
@@ -1,5 +1,6 @@
 import json
-import sys, os
+import sys
+import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "art"))
 import sanity
@@ -15,10 +16,10 @@ def test_log(api):
     config.d_values = [config.A, config.B, config.C]
     config.level.l1_p1.l2_p1.l3_p1 = "test"
     config.level.l1_p2.l4_p1.l1_p2.l4_p1.l1_p1.l2_p1.l3_p1 = "test"
-    api.set_config(config) 
+    api.set_config(config)
 
 
 def test_grpc_log(utils, grpc_api):
     with open(utils.get_test_config_path("config.json")) as f:
         payload = json.load(f)
-    result = grpc_api.set_config(payload)
+    grpc_api.set_config(payload)
