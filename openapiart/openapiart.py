94f8dcc51659f6199a5b5a941865f7eb418e0353
Vibaswan <vroychowdhury@gmail.com>
2023-01-27 20:29:45 +0530

    Segregate proto generation (#386)
    * moving proto generation out of sdk generation
    
    * resolving lint issues
    
    * downgrade ubuntu version to support setup.py
    
    * adding validation of generated yaml and proto in CI
    
    * fix typo
    
    * correcting job nomencleture
    
    * resolving review comments
    
    ---------
    
    Co-authored-by: vhowdhur <vibaswan.roychowdhury@keysight.com>

--- openapiart/openapiart.py
+++ openapiart/openapiart.py
@@ -118,6 +118,10 @@ class OpenApiArt(object):
         except Exception as e:
             print("Bypassed creation of static documentation: {}".format(e))
 
+    def GenerateProtoDef(self, package_name):
+        self._protobuf_package_name = package_name
+        self._generate_proto_file()
+
     def GeneratePythonSdk(self, package_name):
         """Generates a Python UX Sdk
         Args
@@ -142,7 +146,8 @@ class OpenApiArt(object):
         ```
         """
         self._python_module_name = package_name
-        self._generate_proto_file()
+        if not self.proto_file_exsists():
+            self._generate_proto_file()
         if self._python_module_name is not None:
             module = importlib.import_module("openapiart.generator")
             python_ux = getattr(module, "Generator")(
@@ -239,7 +244,8 @@ class OpenApiArt(object):
 
         self._go_sdk_package_dir = package_dir
         self._go_sdk_package_name = package_name
-        self._generate_proto_file()
+        if not self.proto_file_exsists():
+            self._generate_proto_file()
         if self._go_sdk_package_dir and self._protobuf_package_name:
             go_sdk_output_dir = os.path.normpath(
                 os.path.join(
@@ -332,6 +338,15 @@ class OpenApiArt(object):
             }
         )
         protobuf.generate(self._openapi)
+
+    def proto_file_exsists(self):
+        proto_file_path = os.path.normpath(
+            os.path.join(
+                self._output_dir,
+                "{}.proto".format(self._protobuf_package_name),
+            )
+        )
+        return os.path.exists(proto_file_path)
 
     @property
     def output_dir(self):
