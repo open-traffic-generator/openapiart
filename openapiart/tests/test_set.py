ef2f07d724461b7652f17cf1873616e8b1c4fa8a
dipendughosh <dipendughosh@gmail.com>
2023-02-17 19:35:30 +0530

    adding serialize to end of test added 2 negative tests- 1. required property is not set 2. an optional property is set with a value of different type

--- openapiart/tests/test_set.py
+++ openapiart/tests/test_set.py
@@ -24,6 +24,7 @@ def test_set(api):
         list_of_string_values=["abc", "efg"],
         list_of_integer_values=[1, 2, 3, 4],
     )
+    config.required_object.set(e_a=3.1, e_b=4.1)
     config.e.set(e_a=1.1, e_b=2.1)
     config.l.set(
         ipv4="10.1.1.1",
@@ -38,6 +39,11 @@ def test_set(api):
     config.integer_pattern.integer.decrement.set(start=200, step=2, count=100)
 
     config_data = config._properties
+
+    try:
+        config.serialize()
+    except Exception as e:
+        pytest.fail(str(e))
 
     assert config_data[
         "ieee_802_1qbb"
@@ -73,6 +79,14 @@ def test_set(api):
             value == list_of_integer_values[i]
         ), "list_of_integer_values values are not set correctly"
         i = i + 1
+
+    config_required_object_data = config.required_object._properties
+    assert (
+        config_required_object_data["e_a"] == 3.1
+    ), "e_a value is not set correctly"
+    assert (
+        config_required_object_data["e_b"] == 4.1
+    ), "e_b value is not set correctly"
 
     config_e_data = config.e._properties
     assert config_e_data["e_a"] == 1.1, "e_a value is not set correctly"
@@ -115,3 +129,76 @@ def test_set(api):
     assert (
         config_integer_pattern_integer_decrement["count"] == 100
     ), "count value is not set correctly"
+
+    # Negative Test 1 - Not setting required object
+    new_config_1 = sanity.Api().prefix_config()
+    new_config_1.set(
+        ieee_802_1qbb=True,
+        space_1=10,
+        full_duplex_100_mb=20,
+        response=config.STATUS_200,
+        a="abc",
+        b=0.2,
+        c=20,
+        d_values=[config.A],
+        h=False,
+        i="1010100",
+        list_of_string_values=["abc", "efg"],
+        list_of_integer_values=[1, 2, 3, 4],
+    )
+    new_config_1.e.set(e_a=1.1, e_b=2.1)
+    new_config_1.l.set(
+        ipv4="10.1.1.1",
+        ipv6="::",
+        mac="aa:bb:cc:11:22:33",
+        string_param="abcd",
+    )
+    new_config_1.mac_pattern.mac.increment.set(
+        start="aa:aa:bb:bb:cc:cc", step="00:01:00:00:10:00", count=10
+    )
+    new_config_1.integer_pattern.integer.decrement.set(
+        start=200, step=2, count=100
+    )
+
+    try:
+        new_config_1.serialize()
+    except Exception as e:
+        if "required_object is a mandatory property of" not in str(e):
+            pytest.fail("required_object validation failed")
+
+    # Negative Test 2 - Not setting the correct value to property a
+    new_config_2 = sanity.Api().prefix_config()
+    new_config_2.set(
+        ieee_802_1qbb=True,
+        space_1=10,
+        full_duplex_100_mb=20,
+        response=config.STATUS_200,
+        a=10,
+        b=0.2,
+        c=20,
+        d_values=[config.A],
+        h=False,
+        i="1010100",
+        list_of_string_values=["abc", "efg"],
+        list_of_integer_values=[1, 2, 3, 4],
+    )
+    new_config_2.required_object.set(e_a=3.1, e_b=4.1)
+    new_config_2.e.set(e_a=1.1, e_b=2.1)
+    new_config_2.l.set(
+        ipv4="10.1.1.1",
+        ipv6="::",
+        mac="aa:bb:cc:11:22:33",
+        string_param="abcd",
+    )
+    new_config_2.mac_pattern.mac.increment.set(
+        start="aa:aa:bb:bb:cc:cc", step="00:01:00:00:10:00", count=10
+    )
+    new_config_2.integer_pattern.integer.decrement.set(
+        start=200, step=2, count=100
+    )
+
+    try:
+        new_config_2.serialize()
+    except Exception as e:
+        if "property a shall be of type" not in str(e):
+            pytest.fail("a validation failed")
