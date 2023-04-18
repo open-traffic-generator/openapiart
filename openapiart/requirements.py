3a50275a5495278ff72b6517c1852ab1b884ca99
Vibaswan <vroychowdhury@gmail.com>
2023-04-18 14:09:35 +0000

    changing requirements.txt for release compatible versions

--- openapiart/requirements.py
+++ openapiart/requirements.py
@@ -25,18 +25,32 @@ def generate_requirements(path, file_name=None):
     not_required_pkgs = [
         "sanity",
         "typing_extensions",
+        "grpcio-tools~=1.44.0 ; python_version > '2.7'",
+        "grpcio-tools~=1.35.0 ; python_version == '2.7'",
     ]
 
     with open(os.path.join(base_dir, "requirements.txt"), "r") as fd:
         orig_packages = fd.read().splitlines()
+        orig_packages = list(set(orig_packages) - set(not_required_pkgs))
 
     with open(os.path.join(save_path, "requirements.txt"), "r") as fh:
         new_pkgs = fh.read().splitlines()
         new_pkgs = list(set(new_pkgs) - set(not_required_pkgs))
 
-    final_pkgs = new_pkgs
+    with open(
+        os.path.join(os.path.dirname(base_dir), "test_requirements.txt"), "r"
+    ) as fh:
+        test_pkgs = fh.read().splitlines()
+        test_pkgs = list(set(test_pkgs) - set(not_required_pkgs))
+
+    final_pkgs = []
     for n_pkg in new_pkgs:
         for pkg in orig_packages:
+            if n_pkg in pkg and pkg not in final_pkgs:
+                final_pkgs.append(pkg)
+
+    for n_pkg in new_pkgs:
+        for pkg in test_pkgs:
             if n_pkg in pkg and pkg not in final_pkgs:
                 final_pkgs.append(pkg)
 
