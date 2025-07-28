ed8d7d34b6176fadd636e72540a59c00883ee38d
Vibaswan <vroychowdhury@gmail.com>
2025-07-28 19:06:06 +0530

    v0.3.29

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "openapiart"
-version = "0.3.28"
+version = "0.3.29"
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(base_dir, "readme.md")) as fid:
